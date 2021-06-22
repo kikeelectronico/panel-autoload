ed89d8e102bd8dd4489582838115a583eb80577b
enrique.gomez <enrique.gomez@bq.com>
2021-06-22 21:30:04 +0200

    code

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # panel-autoload
+
+Panel autoload loads a web page on a Chromecast device automatically when the Chromecast enter in Ambient Mode.
+
+# Parameters
+
+NAME: The name of the target Chromecast.
+TRIGGER_APP: The name of the app that trigger the autoload. Backdrop is the Ambient Mode default app.
+WEB_PANEL: The URL to the web page that must be loaded.
+TEST: Never Gonna Give You Up music video.
---
+++ main.py
@@ -0,0 +1,20 @@
+import pychromecast
+import os
+
+NAME = "Chromecast"
+TRIGGER_APP = "Backdrop"
+WEB_PANEL = "http://192.168.10.2:8080"
+TEST = False
+
+# Get the actual app
+chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[NAME])
+cast = chromecasts[0]
+cast.wait()
+
+# It is the trigger app?
+if cast.status.display_name == TRIGGER_APP:
+  os.system("catt -d " + NAME + " set_default")
+  if TEST:
+    os.system("catt cast \"https://www.youtube.com/watch?v=dQw4w9WgXcQ\"")
+  else:
+    os.system("catt cast_site \"" + WEB_PANEL + "\"")
