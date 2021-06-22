3cc279c53653a582bb68f261be93302c5566907d
enrique.gomez <enrique.gomez@bq.com>
2021-06-22 21:36:42 +0200

    loop

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import pychromecast
 import os
+import time
 
 NAME = "Chromecast"
 TRIGGER_APP = "Backdrop"
 WEB_PANEL = "http://192.168.10.2:8080"
 TEST = False
+LOOP = True
 
-# Get the actual app
-chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[NAME])
-cast = chromecasts[0]
-cast.wait()
+while LOOP:
+  # Get the actual app
+  chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[NAME])
+  cast = chromecasts[0]
+  cast.wait()
 
-# It is the trigger app?
-if cast.status.display_name == TRIGGER_APP:
-  os.system("catt -d " + NAME + " set_default")
-  if TEST:
-    os.system("catt cast \"https://www.youtube.com/watch?v=dQw4w9WgXcQ\"")
-  else:
-    os.system("catt cast_site \"" + WEB_PANEL + "\"")
+  # It is the trigger app?
+  if cast.status.display_name == TRIGGER_APP:
+    os.system("catt -d " + NAME + " set_default")
+    if TEST:
+      os.system("catt cast \"https://www.youtube.com/watch?v=dQw4w9WgXcQ\"")
+    else:
+      os.system("catt cast_site \"" + WEB_PANEL + "\"")
+  time.sleep(30)
