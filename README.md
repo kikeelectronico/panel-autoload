91211f3dc95e81b4ddc0448d375e97c979e7a642
enrique.gomez <enrique.gomez@bq.com>
2021-06-29 20:18:28 +0200

    a loop wasn't a good idea

--- README.md
+++ README.md
@@ -12,32 +12,11 @@ Panel autoload loads a web page on a Chromecast device automatically when the Ch
 
 *TEST:* Never Gonna Give You Up music video.
 
-*LOOP:* Run indefinitely
 
+# How to install
 
-# How to install and run on Debian and similar OS
-
-1. Install the dependencies
 ```
 sudo pip3 install -r requirements.txt
-```
-2. Change the working directory in *panel-autolaod.service*.
-
-3. Move the service to the system directory
-```
-sudo cp panel-autoload.service /lib/systemd/system
-```
-4. Reload the daemon
-```
-sudo systemctl daemon-reload
-```
-5. Enable the service
-```
- sudo systemctl enable panel-autoload.service
-```
-6. Start the service
-```
- sudo systemctl start panel-autoload.service
 ```
 
 
--- main.py
+++ main.py
@@ -6,21 +6,18 @@ NAME = "Chromecast"
 TRIGGER_APP = "Backdrop"
 WEB_PANEL = "http://192.168.10.2:8080"
 TEST = False
-LOOP = True
 
-while LOOP:
-  # Get the actual app
-  chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[NAME])
-  if len(chromecasts) > 0:
-    cast = chromecasts[0]
-    cast.wait()
+# Get the actual app
+chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[NAME], retry_wait=0, discovery_timeout=2)
+if len(chromecasts) > 0:
+  cast = chromecasts[0]
+  cast.wait()
 
-    # It is the trigger app?
-    if cast.status.display_name == TRIGGER_APP:
-      os.system("catt -d " + NAME + " set_default")
-      if TEST:
-        os.system("catt cast \"https://www.youtube.com/watch?v=dQw4w9WgXcQ\"")
-      else:
-        os.system("catt cast_site \"" + WEB_PANEL + "\"")
-  if LOOP:
-    time.sleep(30)
+  # It is the trigger app?
+  if cast.status.display_name == TRIGGER_APP:
+    os.system("catt -d " + NAME + " set_default")
+    if TEST:
+      os.system("catt cast \"https://www.youtube.com/watch?v=dQw4w9WgXcQ\"")
+    else:
+      os.system("catt cast_site \"" + WEB_PANEL + "\"")
+pychromecast.discovery.stop_discovery(browser)
